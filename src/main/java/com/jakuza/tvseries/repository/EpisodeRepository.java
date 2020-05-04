7632a784bb95fe63a6602182d42830a214e0e790
Zoltan Jakubecz <jakuzo@gmail.com>
2020-05-04 14:04:23 +0200

    WIP

--- src/main/java/com/jakuza/tvseries/repository/EpisodeRepository.java
+++ src/main/java/com/jakuza/tvseries/repository/EpisodeRepository.java
@@ -4,4 +4,5 @@ import com.jakuza.tvseries.entity.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
+    
 }
